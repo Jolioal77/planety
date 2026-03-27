291768a3a531992d8f8280d8a7c06660b8ce121b
Jolioal77 <alvin.binse77@gmail.com>
2026-03-27 09:07:54 +0200

    Latest Code

--- esp32.ino
+++ esp32.ino
@@ -1,10 +1,10 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <ArduinoJson.h> 
-#include "DHT.h"          
+#include "DHT.h"
 
-const char* ssid = "Enko_Students";    // Change this to YOUR Wifi Name
-const char* password = "Enk0@234";     // Change this to YOUR Wifi Password
+const char* ssid = "Enko_Students";
+const char* password = "Enk0@234";
 
 #define DHTPIN 27         
 #define DHTTYPE DHT11     
@@ -15,22 +15,29 @@ DHT dht(DHTPIN, DHTTYPE);
 WebServer server(80);
 
 void handleData() {
-  // 1. Read Raw Values
   float h = dht.readHumidity();
   float t = dht.readTemperature(); 
   int soilRaw = analogRead(SOIL_PIN);
   int lightRaw = analogRead(LIGHT_PIN);
 
-  float s = map(soilRaw, 3200, 1500, 0, 100);
-  s = constrain(s, 0, 100); // Keep it between 0 and 100%
+  
+  Serial.print("Raw Soil: "); Serial.print(soilRaw);
+  Serial.print(" | Raw Light: "); Serial.println(lightRaw);
 
+  
+  float s = map(soilRaw, 3200, 1000, 0, 100); 
+  s = constrain(s, 0, 100); 
+
+  
   float l = map(lightRaw, 0, 4095, 0, 1000); 
   l = constrain(l, 0, 1000);
 
+  
   if (isnan(h) || isnan(t)) {
     t = 0.0; h = 0.0;
   }
 
+  
   StaticJsonDocument<200> doc;
   doc["t"] = t;
   doc["h"] = h;
@@ -40,6 +47,7 @@ void handleData() {
   String jsonResponse;
   serializeJson(doc, jsonResponse);
 
+  
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "application/json", jsonResponse);
 }
@@ -48,17 +56,16 @@ void setup() {
   Serial.begin(115200);
   dht.begin();
   
-  
   analogReadResolution(12);
 
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
+  Serial.print("Connecting to Planety Network");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("\n--- CONNECTED ---");
+  Serial.println("\n--- SYSTEM ONLINE ---");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP()); 
 
